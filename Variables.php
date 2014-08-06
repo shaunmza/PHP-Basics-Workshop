c0a99e47ac5ff416fb610319285d8b3e0dcd6fbe
Shaun Morrow <morrow.shaun@gmail.com>
2014-08-06 20:47:13 +0200

    First version of Variables.php

---
+++ Variables.php
@@ -0,0 +1,25 @@
+<?php
+// Variables
+// Variable names must start with an underscore _ or a letter
+// $123 is NOT a valid variable name
+// After that the name can contain letters, numbers and underscores
+$a = 'Test';
+
+// We can display variables using the echo function
+//echo $a;
+
+// Or print them out
+//print($a);
+
+$b1 = '123';
+
+// Variables can be echo'ed in a string
+//echo "$a $b1";
+
+// or printed
+//print("$a $b1");
+
+// adding variables could have unexpected outcomes
+//echo $a + $b1;
+// Why does this happen?
+?>
